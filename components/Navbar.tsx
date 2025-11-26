db97f3a19ace626332476a83d43bba8655e7f8de
Mudassar Aulakh <mudassarhussain4047769@gmail.com>
2025-11-26 21:11:17 +0500

    navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <div className="bg-[#0A0A0A]">
       <nav
-        className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 w-full mx-auto ${
+        className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 w-full px-10 bg-black ${
           open ? "bg-black md:bg-transparent" : ""
         }`}
       >
